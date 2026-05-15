8837c85ee90a35dc97606b8d5f7c27e6790cc726
Gareth Noble <garethnoble3@gmail.com>
2026-05-14 23:08:56 -0400

    docs: removed unecessary comments

--- sms-api/services/notiServices.js
+++ sms-api/services/notiServices.js
@@ -6,13 +6,10 @@ const saveNotification = async (notificationData) => {
 };
 
 const fetchLatest = async (dateString) => {
-    // 1. Get the target date as a string (defaults to today if undefined)
     const targetDateStr = new Date(dateString || Date.now()).toDateString();
 
-    // 2. Fetch all unread messages for that specific date straight from the DB
     const messages = await messageRepo.findUnread(targetDateStr);
 
-    // 3. Format for desktop overlay
     return messages.map(msg => ({
         id: msg._id,
         app_package: msg.app_package,
